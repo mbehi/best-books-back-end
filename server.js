26f4a4089b692298d3e28efc0c9d9aabc49729cf
aloysious x <alicia.todd.army@gmail.com>
2021-05-01 14:07:20 -0700

    debuggedServer

--- server.js
+++ server.js
@@ -16,40 +16,40 @@ require('dotenv').config();
 // hey mongoose, connect to the database at localhost:27017
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 // I’m intentionally requiring this model AFTER I run mongoose.connect
-const {User, userBookProfile} = require('./models/User');
+const User = require('./models/User');
 // seed the database with some books, so I can retrieve them
-const userBookProfile = new User({
+const userProfile = new User({
   
-  userEmail: 'Aloysious',
+  userEmail: 'aloysiousx@gmail.com',
   description: '',
   status: '',
   favoriteBooks: [
     { bookName: 'The Giver' },
     { bookName: 'Beyond Inclusion, Beyond Empowerment' }, { bookName: 'The secret life of bees' }
   ]
-}); userBookProfile.save(function (err) {
-  if (err) {
-    console.err(err);
-  } else {
-    console.log('saved the book');
-  }
-});
+}); 
+
+userProfile.save().then(() => console.log('successfully saved'));
+
+// console.log(User);
 app.get('/', (request, response) => {
   response.send('hello, cool book!');
 });
-// app.get('/books', (request, response) => {
-//   // get all the books from the database
-//   userBookProfile.find((err, databaseResults) => {
-//     // send them in my response
-//     response.send(databaseResults);
-//   });
-// });
+
+// i am unsure about this line of code
+app.get('/books', (request, response) => {
+  // get all the books from the database
+  User.find((err, databaseResults) => {
+    // send them in my response
+    response.send(databaseResults[0]);
+  });
+});
 
 // route to get just one book
 app.get('/book', (request, response) => {
   // set the user to access the query and email stuffs
-  let user = req.query.user;
- userBookProfile.find({userEmail: user }, (err, databaseResults) => {
+  let user = request.query.user;
+ User.find({userEmail: user }, (err, databaseResults) => {
   // send them in my response 
   //  [results at zero for just the dataaaaaaaa]
     response.send(databaseResults[0]);
