f93653e2af6f0903bd745d31bf5669e4121e3559
aloysious x <alicia.todd.army@gmail.com>
2021-05-05 16:56:08 -0700

    broken server fixed

--- README.md
+++ README.md
@@ -7,11 +7,14 @@
 <a href="https://trello.com/b/qrG2StmE/am-best-books" align="center">📚 Best Books Trello board</a>
 
 ## Overview
+
 We will be creating a small app to track what books have impacted you, and what's recommended to read next.
 This server will gather boook informationg from the Mongoose database
+
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
 
 ## Getting Started
+
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
 ## Architecture
@@ -21,10 +24,13 @@ This server will gather boook informationg from the Mongoose database
 **Dependencies :** Mongoose | Express
 
 **Tools :** VS Code | GitHub | Trello | Slack
+
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
 
 ## Change Log
+
 04-24-2021 0:00pm ** Deployed from Heroku** · v1 · a000aaa0
+
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
 01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
 
@@ -57,24 +63,22 @@ Actual time needed to complete: 46 minutes
 > As a user, I'd like my books to persist, so that whenever I make changes, I can see the my full list of best books.As a user, I'd like my books to persist, so that whenever I make changes, I can see the my full list of best books.
 
 ```sh
-Estimate of time needed to complete: _____
-Start time: _____
-Finish time: _____
-Actual time needed to complete: _____
+Estimate of time needed to complete: 120 mins
 ```
 
 ## ⚙️ Book Component : Feature Three
+
 > As a user, I'd like to see my list of books, so that I can track what's impacted me, and what's recommended to me.
 
 ```sh
-Estimate of time needed to complete: _____
-Start time: _____
-Finish time: _____
-Actual time needed to complete: _____
+Estimate of time needed to complete: 120 mins
 ```
 
 <h2 align="center"> ❤️️ Credit and Collaborations 👥 </h2>
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
++ Props to TA Kristain who helped preserve our data through the use of `https://cloud.mongodb.com/`
++ Props to TA Paul for the time spent assisting with the debugging of this codebbase
 
 ## Resources
 
--- server.js
+++ server.js
@@ -12,53 +12,62 @@ require('dotenv').config();
 // will need later
 const cors = require('cors');
 app.use(cors());
-
-// hey mongoose, connect to the database at localhost:27017
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-// I’m intentionally requiring this model AFTER I run mongoose.connect
-const User = require('./models/User');
-const { getMaxListeners } = require('./models/User');
-// seed the database with some books, so I can retrieve them
-const userProfile = new User({
   
-  userEmail: 'aloysiousx@gmail.com',
-  favoriteBooks: [
-    { name: 'The Giver' },
-    { name: 'Beyond Inclusion, Beyond Empowerment' }, { name: 'The secret life of bees' }
-  ]
-});
-
-userProfile.save().then(() => console.log('successfully saved'));
-
-// console.log(User);
-app.get('/', (request, response) => {
-  response.send('hello, cool book!');
-});
-
-// i am unsure about this line of code
-app.get('/books', (request, response) => {
-  // get all the books from the database
-  User.find((err, databaseResults) => {
-    console.log(databaseResults);
-    // send them in my response
-    response.send(databaseResults[-1]);
+  const User = require('./models/User');
+  
+  // seed the database with some books, so I can retrieve them
+  const userProfile = new User({
+    
+    userEmail: 'aloysiousx@gmail.com',
+    favoriteBooks: [
+      { name: 'The Giver' },
+      { name: 'Beyond Inclusion, Beyond Empowerment' }, { name: 'The secret life of bees' }
+    ]
   });
-});
-
-// route to get just one book
-app.get('/book', (request, response) => {
-  // set the user to access the query and email stuffs
-  let user = request.query.user;
-User.find({userEmail: 'aloysiousx@gmail.com' }, (err, databaseResults) => {
-  //  [results at zero for just the dataaaaaaaa]
-  // send them in my response
-    response.send(databaseResults[0]);
+  
+  userProfile.save().then(() => console.log('successfully saved'));
+  
+  // console.log(User);
+  app.get('/', (request, response) => {
+    response.send('hello, cool book!');
   });
+  
+  
+  app.get('/books', (request, response) => {
+    // get all the users from the database
+    let emailFromRequest = request.params
+    User.find({},(error, databaseResults) => {
+      console.log(databaseResults, 'in /books route');
+      // send them in my response
+      response.send(databaseResults[0]);
+    });
+  });
+  
+  // route to get just one book
+  app.get('/book', (request, response) => {
+    // set the user to access the query and email stuffs
+    let user = request.query.user;
+    User.find({userEmail: 'aloysiousx@gmail.com' }, (error, databaseResults) => {
+      //  [results at zero for just the dataaaaaaaa]
+      // send them in my response
+      response.send(databaseResults[0]);
+    });
+  });
+  
+  
+  const PORT = process.env.PORT || 3001;
+  
+  console.log('❤️ hello aloysious & Mohsin . . . welcome to the back-end! ❤️');
+  // hey mongoose, connect to the database at localhost:27017
+  // I’m intentionally requiring this model AFTER I run mongoose.connect
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // Was assisted by Kristian who was able to get the server to hold data and save by using = https://cloud.mongodb.com/
+  // we created a new env variable to house the data. 
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> {
+  console.log('connected to mongodb');
+  app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+}).catch((e) => {
+  console.log('error:', e);
 });
 
-
-const PORT = process.env.PORT || 3001;
-
-console.log('❤️ hello aloysious & Mohsin . . . welcome to the back-end! ❤️');
-
-app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
